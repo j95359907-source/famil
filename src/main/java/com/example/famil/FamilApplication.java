0f9a1dd084b8691df8e265606ddb60e1de347627
j95359907-source <j95359907@gmail.com>
2026-04-03 02:03:25 +0800

    提交

---
+++ src/main/java/com/example/famil/FamilApplication.java
@@ -0,0 +1,13 @@
+package com.example.famil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/famil/FamilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.famil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
